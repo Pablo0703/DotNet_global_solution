c2dd20b16ccdbccaa32579e6776111a0720d2daa
Pablo0703 <lopespablo545@gmail.com>
2025-06-04 21:27:03 -0300

    Limpeza de arquivos temporários e configuração correta do .gitignore

--- Global Solution/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Global_Solution.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
